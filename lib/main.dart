58452c8e63288c4169a83dce4cf0a668e8215c74
mahdiramazani <mahdiramazani1281@gmail.com>
2024-01-27 22:28:58 +0330

    add new fiture

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,52 @@ void main() {
   runApp(GetAplication());
 }
 
-class GetAplication extends StatelessWidget{
-
+class GetAplication extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-
+  Widget build(BuildContext context) {
     return GetBody();
   }
 }
 
-
-Widget GetBody(){
-
-
+Widget GetBody() {
   return MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(fontFamily: "vazir"),
-    home: Scaffold(body: SafeArea(
-      child: Text("مهدی رمضانی")
+    home: Scaffold(
+      appBar: getAppBar(),
+      body: SafeArea(
+        child: getbody(),
       ),
-      ),
+    ),
+  );
+}
+
+PreferredSizeWidget getAppBar() {
+  return AppBar(
+    elevation: 0,
+    backgroundColor: Colors.amber,
+    title: Center(
+      child: Text("مهدی رمضانی هستم"),
+    ),
+  );
+}
+
+Widget getbody() {
+  return SingleChildScrollView(
+    child: Column(
+      children: [
+        Center(
+          child: CircleAvatar(
+            backgroundColor: Colors.black,
+            radius: 70,
+            // backgroundImage: AssetImage("assetName"),
+          ),
+        ),
+        Text(
+          "من مهدی رمضانی هستم برنامه نویس فلاتر",
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w700),
+        ),
+      ],
+    ),
   );
 }
