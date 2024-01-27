02681dcc82ebfffed4fe62da497b800a6aa60f8a
mahdiramazani <mahdiramazani1281@gmail.com>
2024-01-27 18:27:48 +0330

    initial

---
+++ android/app/src/main/kotlin/com/example/porfolito/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.porfolito
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GetAplication());
+}
+
+class GetAplication extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+
+    return GetBody();
+  }
+}
+
+
+Widget GetBody(){
+
+
+  return MaterialApp();
+}
